1d9ef7705654f8c614774aca337526b72245fc60
Vivek Nath <er.viveknath@outlook.com>
2024-10-07 11:52:37 +0530

    All files have added

---
+++ src/main/java/Oct02102024/SimpleCalculator.java
@@ -0,0 +1,13 @@
+package Oct02102024;
+
+public class SimpleCalculator
+{
+    public static void main(String[] args)
+    {
+        System.out.println(10+2);
+        System.out.println(10-3);
+        System.out.println(10*4);
+        System.out.println(10/3);
+
+    }
+}
