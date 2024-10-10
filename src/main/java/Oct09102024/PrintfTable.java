1ad2091b75a53cf089d987c9347cb1aae0f619b3
Vivek Nath <er.viveknath@outlook.com>
2024-10-10 18:09:35 +0530

    All files have added

---
+++ src/main/java/Oct09102024/PrintfTable.java
@@ -0,0 +1,21 @@
+package Oct09102024;
+
+public class PrintfTable
+{
+    public static void main(String[] args)
+    {
+        String name="Shivani";
+        int age= 21;
+        String gender="female";
+        System.out.printf("%s%n",name);
+
+        System.out.printf("%d%n",age);
+        System.out.printf("%s%n",gender);
+
+
+
+
+    }
+
+}
+
