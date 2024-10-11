3d4a7a0e02af1d70b51a962b94476a1aea2f8b86
Vivek Nath <er.viveknath@outlook.com>
2024-10-11 19:56:45 +0530

    All files have added

---
+++ src/main/java/Oct11102024/Arithmatic.java
@@ -0,0 +1,16 @@
+package Oct11102024;
+
+public class Arithmatic
+{
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println(10*10/5+3-1*4/2);
+
+    }
+}
---
+++ src/main/java/Oct11102024/Operator1.java
@@ -0,0 +1,13 @@
+package Oct11102024;
+
+public class Operator1
+{
+    public static void main(String[] args) {
+        int x=10;
+        System.out.println(x++);
+        System.out.println(++x);
+        System.out.println(x--);
+        System.out.println(--x);
+
+    }
+}
