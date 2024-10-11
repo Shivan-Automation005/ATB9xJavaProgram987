0359ad2c165e950f520d24658004db01462dedc9
Vivek Nath <er.viveknath@outlook.com>
2024-10-11 11:51:22 +0530

    All files have added

---
+++ src/main/java/Oct11102024/Operators.java
@@ -0,0 +1,13 @@
+package Oct11102024;
+
+public class Operators
+{
+    public static void main(String[] args)
+    {
+        int a=12;
+        boolean b=!(a>10||a<5);
+        System.out.println(b);
+
+    }
+}
+
